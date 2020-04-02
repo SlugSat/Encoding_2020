27fcfcad0efdc65e1d9ce5b8831f30feebb94b72
unknown <chscalde@ucsc.edu>
2020-04-01 23:25:59 -0700

    De-interweaver seems to work properly. Could posibly be optimized. Need to re-assest program in the future.

--- Turbo_BCH/TurboEncoderBCH.c
+++ Turbo_BCH/TurboEncoderBCH.c
@@ -7,6 +7,7 @@
 int intro(int *n, int *k, int d, int argc, char *argv[]);
 
 u_int32_t inter_leaver(u_int32_t input);  //Scrambles the data so that burst error become single errors
+u_int32_t de_inter_leaver(u_int32_t input);
 
 int main(int argc, char *argv[])
 {
@@ -22,6 +23,8 @@ u_int32_t test = n;
 printf("Recieved is %d\n",test);
 test = inter_leaver(test);
 printf("Output is %d\n",test);
+test = de_inter_leaver(test);
+printf("---------Input Num is %d\n",test);
 
 }
 
@@ -142,23 +145,29 @@ u_int32_t inter_leaver(u_int32_t input){
 u_int32_t de_inter_leaver(u_int32_t input){
 
     u_int32_t output = 0;
-    u_int8_t number [4];
-                                                //Shifts data so that all bits are on the last 8 bits
-    number[0] = 15 ^ input;             //25|17| 9| 1|24|16| 8| 0|                                
-    number[1] = (240 ^ input)   >>4;    //27|19|11| 3|26|18|10| 2|
-    number[2] = (3840 ^ input)  >>8;    //29|21|13| 5|28|20|12| 4|
-    number[3] = (61440 ^ input) >>12;   //31|23|15| 7|30|22|14| 6|
+    u_int8_t number [8];
+                                            //Shifts data so that all bits are on the last 8 bits
+    number[0] = 15 & input;                 //25|17| 9| 1|24|16| 8| 0|                                
+    number[1] = (240 & input)        >>4;   //27|19|11| 3|26|18|10| 2|
+    number[2] = (3840 & input)       >>8;   //29|21|13| 5|28|20|12| 4|
+    number[3] = (61440 & input)      >>12;   //31|23|15| 7|30|22|14| 6|
+    number[4] = (983040 & input)     >>16;
+    number[5] = (15728640 & input)   >>20; 
+    number[6] = (251658240 & input)  >>24; 
+    number[7] = (4026531840 & input) >>28; 
 
-        for (int x = 0; x < 8; x++)
+        for (int x = 0; x < 4; x++)
         {
 
-        output = output + (number[0] ^ (1 <<  x )) << (4*x);
-        output = output + (number[1] ^ (1 <<  x )) << (4*x + 1);
-        output = output + (number[2] ^ (1 <<  x )) << (4*x + 2);
-        output = output + (number[3] ^ (1 <<  x )) << (4*x + 3);
-
-
+        output =  output + ((number[0] & (1 << x)) << (8*x));
+        output =  output + ((number[1] & (1 << x)) << (1 + 8*x ));
+        output =  output + ((number[2] & (1 << x)) << (2 + 8*x ));
+        output =  output + ((number[3] & (1 << x)) << (3 + 8*x ));
+        output =  output + ((number[4] & (1 << x)) << (4 + 8*x ));
+        output =  output + ((number[5] & (1 << x)) << (5 + 8*x ));
+        output =  output + ((number[6] & (1 << x)) << (6 + 8*x ));
+        output =  output + ((number[7] & (1 << x)) << (7 + 8*x ));
         }
     
-    return output;  //Returns 0|1|2|3|4|5|6|7|8
+    return output;  //Returns 31|30|29|28|27|26|25|24|23|22|21|20|19|18|...
 }
