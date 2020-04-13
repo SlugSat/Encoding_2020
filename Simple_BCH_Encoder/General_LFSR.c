e70c2dbcaec69244f1052159da866fdb2d895fcc
Christopher <chscalde@ucsc.edu>
2020-04-13 02:59:11 -0700

    Looks like BCH might work. More testing needed to verify

--- Simple_BCH_Encoder/General_LFSR.c
+++ Simple_BCH_Encoder/General_LFSR.c
@@ -13,11 +13,11 @@
 #include <unistd.h> 
 
 int main(int argc, char *argv[]){
-    u_int32_t inits = 03551; //In C 0 signifies an octal number
+    u_int32_t inits = 013;//03551; //In C 0 signifies an octal number
                             //The octal number specifies the desired 
                             //LSFR structure.
-    u_int32_t input_bits = 1287565; // data bits for LFSR
-    u_int8_t repetitions = 32;// Connected to the number of registers in 
+    u_int32_t input_bits = 11;//1455289;//1287565; // data bits for LFSR
+    u_int8_t repetitions = 7;// Connected to the number of registers in 
                             //The LFSR structure
     u_int32_t parity_bits = 0;//Container for parity bit
 
