881b94862cd1e08b1b2f7fe0a2520acc5392c372
unknown <chscalde@ucsc.edu>
2020-04-21 19:02:01 -0700

    taking break on verification

--- Simple_BCH_Encoder/BCH_Encoder.c
+++ Simple_BCH_Encoder/BCH_Encoder.c
@@ -13,83 +13,46 @@
 #include <ctype.h>
 #include <unistd.h> 
 
-
-u_int8_t lmb(u_int32_t input);
-u_int32_t make_mask(u_int32_t input,u_int8_t left_most_bit);
+int print_binary (u_int32_t register);
 
 int main(int argc, char *argv[]){
-
-    int n = 31;
-    int k = 21;
-
-    u_int32_t structure = 03551; //In C 0 signifies an octal number
+    u_int32_t inits = 03551; //In C 0 signifies an octal number  11101101001
                             //The octal number specifies the desired 
                             //LSFR structure.
-    u_int32_t test_structure = 03551;
-    u_int8_t left_most_bit = lmb(structure);
-    structure = make_mask(structure,left_most_bit);
-
-    u_int32_t toggel_mask = 0b1 << left_most_bit;
-    //test_structure = structure ^ toggel_mask;
-    test_structure = test_structure >> 1;
-
-    printf("New structure %d\n", test_structure);
-
-    u_int32_t input_bits = 11;//data bits for LFSR
-    u_int8_t repetitions = 31;//Number of bits to be coded  K paramter
-    u_int32_t code_word = 0;//Container for codeword
-    u_int32_t registers = n-k-1;//Represents the current state of the registers
-
-
-    u_int32_t reg_output_mask = 0b1 << registers;
+    u_int32_t input_bits = 69;//1455289;//1287565; // data bits for LFSR
+    u_int8_t repetitions = 21;// Connected to the number of registers in 
+                            //The LFSR structure
+    u_int32_t parity_bits = 0;//Container for parity bit
 
     for(int x = 0; x < repetitions; x++){
 
-        u_int8_t reg_output = reg_output_mask & code_word; //Get value in last register
-        u_int8_t input = 0b1 & input_bits; //Get input bit from input
-        input = input ^ reg_output;
-        input_bits = input_bits >> 1;
-
-
-        
-        // code_word = code_word + ( << x);
-        // u_int32_t mask = (~() + 1) & structure;     //Performs 2's complement
-        // printf("Input %d Mask %d\n",input_bits,mask);
-        // input_bits = (input_bits >> 1) ^ mask;   //Gets next state of LFSR 
+        //Compute input value
+        u_int8_t parity_bit = 0b1 & input_bits;
+        u_int32_t outputbit = (parity_bits & 01000) >> 9;
+        u_int32_t xor_val = parity_bit ^ outputbit;
+        //Get xor mask
+        u_int32_t mask = (~(parity_bit) + 1) & inits;   //Performs 2's complement
+        parity_bits = (parity_bits ^ mask) << 1;        //Apply xor mask
+        parity_bits = parity_bits + xor_val;            //Shift in input bit
+        print_binary(parity_bits);
+        printf(" Input %d Mask %d Register state %d\n",xor_val,mask,parity_bits);
     }
-
-    printf("Code Word are: %d",code_word);
-}
-
-u_int8_t lmb(u_int32_t input){
-
-    u_int32_t copy = input;
-    u_int8_t left_most_bit = 0;
-
-     //Get left most bit
-    for(int x = 0; x < 31;x++){
     
-    u_int8_t bit = copy & 0b1;
-    copy = copy >> 1;
-    if(bit) left_most_bit = x;
 
-    }
-    printf("Left most bit is in place: %d\n",left_most_bit);
-    return left_most_bit;
+    printf("Parity bits are: %d",parity_bits);
 }
 
-u_int32_t make_mask(u_int32_t input,u_int8_t left_most_bit){
 
-    u_int32_t output = 0;
+int print_binary (u_int32_t rg){
 
-    for(int cp = 0; cp <= left_most_bit; cp++){
+    u_int32_t bit = 010000000000; 
 
-        output = output << 1;
-        u_int8_t bit = input & 1;
-        output = output + bit;
-        input = input >> 1;
+    for(int x = 0; x < 31;x++){00
+
+    if(bit & rg) printf("1");
+    else printf("0");
+    bit = bit >> 1;
 
     }
-    printf("Final structure: %d\n",output);
-    return output;
+
 }
